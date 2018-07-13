998e8ae0acaaec0e2feb71476c02000549b300bd
Melina Mejia Bedoya <melinamejia95@gmail.com>
2018-07-12 19:08:27 -0500

    Register component form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConnectionService } from '../service/connection.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  rForm: FormGroup;
+  titleAlert: string = "Campo requerido";
+  conect: any = [];
+
+  constructor(private fb: FormBuilder, private _register: ConnectionService) { 
+
+    this.rForm = fb.group({
+      'id': [null, Validators.required],
+      'nombre': [null, Validators.required],
+      'apellido': [null, Validators.required],
+    });
+
+  }
 
   ngOnInit() {
+  }
+
+  registerUser(post){
+    console.log(post.nombre)
+    if (post) {    
+      this.conect = {'id': post.id, 'nombre': post.nombre, 'apellido': post.apellido};
+      this._register.signin(this.conect).subscribe(data =>{
+        if(data = "Registro creado"){
+          swal({
+            title: 'Registro creado con éxito',
+            text: '',
+            type: 'success',
+            onClose: function reload() {
+                      location.reload();
+                    }
+          })
+        }
+      },
+      error => {
+        swal(
+          'Error con tu nombre de usuario y/o contraseña',
+          '',
+          'warning'
+        )
+        
+      });
+    }
   }
 
 }
